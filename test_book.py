660e53e64f3f12c8416e14b034d3e1aba546741b
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 22:35:56 +0530

    done

--- test_book.py
+++ test_book.py
@@ -4,18 +4,21 @@ from book import BookStore
 
 class BookTest(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.book_store = BookStore()
+
+    def tearDown(self) -> None:
+        pass
+
     def test_get_by_title(self):
-        book_store = BookStore()
-        book_store.create_book('War and Peace', '12345')
-        id = book_store.get_id_by_title('War and Peace')
+        self.book_store.create_book('War and Peace', '12345')
+        id = self.book_store.get_id_by_title('War and Peace')
         self.assertEqual('12345', id)
 
     def test_absent_title(self):
-        book_store = BookStore()
         with self.assertRaises(KeyError):
-            id = book_store.get_id_by_title('The Idiot')
+            id = self.book_store.get_id_by_title('The Idiot')
 
     @unittest.skip("Under development")
     def test_empty_store_has_integrity(self):
-        book_store = BookStore()
-        self.assertTrue(book_store.has_integrity())
+        self.assertTrue(self.book_store.has_integrity())
