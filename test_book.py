33c7f0d7c6e72ebdd978b35e97f43a970c64d213
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-14 22:17:29 +0530

    done

--- test_book.py
+++ test_book.py
@@ -14,3 +14,8 @@ class BookTest(unittest.TestCase):
         book_store = BookStore()
         with self.assertRaises(KeyError):
             id = book_store.get_id_by_title('The Idiot')
+
+    @unittest.skip("Under development")
+    def test_empty_store_has_integrity(self):
+        book_store = BookStore()
+        self.assertTrue(book_store.has_integrity())
