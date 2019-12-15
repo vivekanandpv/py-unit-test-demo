f6c698d19766ce6c6a2a7cc41cd52174d2add834
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-15 09:52:52 +0530

    done

--- test_book.py
+++ test_book.py
@@ -2,14 +2,17 @@ from book import BookStore
 from pytest import *
 
 
-def test_get_by_title():
-    book_store = BookStore()
+@fixture
+def book_store():
+    return BookStore()
+
+
+def test_get_by_title(book_store):
     book_store.create_book('War and Peace', '12345')
     id = book_store.get_id_by_title('War and Peace')
     assert '12345' == id
 
 
-def test_absent_title():
-    book_store = BookStore()
+def test_absent_title(book_store):
     with raises(KeyError):
         id = book_store.get_id_by_title('The Idiot')
